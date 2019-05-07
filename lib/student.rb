ef1acb2978ecd87cbed6632d0e5d5de851a5196a
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-07 00:53:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,15 @@ class Student
     @grade = grade 
   end 
   
-  def create_table 
+  def self.create_table 
+     sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade FLOAT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end 
   
   
