5f69218a4cdd408dec94fac4ca31e74add53cde6
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-07 00:49:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  def initialize(name:, grade:)
+  def initialize(name:, grade:, id: )
     @name = name 
     @grade = grade 
   end 
